14ce1d6ecb6e74491f568e2e78adb25e70f68133
Daws18 <dawsonfernandes18@gmail.com>
2023-10-12 13:36:53 +1300

    added asset and fixed other issues created function

--- music_starter.js
+++ music_starter.js
@@ -1,93 +1,128 @@
+let bullet;
+let firstrun = true
+let bullet1x = -100
+let bullet1y = 100
+let bullet2x = -150
+let bullet2y = 150
+let bullet3x = -300
+let bullet3y = 300 
+let bullet4x = -500
+let bullet4y = 500
+
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  if (firstrun) {
+    // Bullet image size is 500x250
+    bullet = loadImage('assets/Bullet.png', img => {print("Image loaded")}, img => {print("Failed to load")})
+    
+    firstrun = false
+  }
   background(20)
   textFont('Helvetica'); // please use CSS safe fonts
   //rectMode(CENTER)
   ellipseMode(CENTER)
   textSize(24);
 
+  // console.log(song.currentTime())
+  fill(255)
+  text(song.currentTime(), width / 2, height / 3);
+  //start of song 
 
-  volumeBar(bass,color(240, 34, 19),color(240, 137, 19),0)//red to orange maping bass
-  volumeBar(drum,color(19, 240, 63),color(19, 100, 240),100)//green to blue maping drum
-  volumeBar(vocal,color(19, 240, 63),color(19, 100, 240),150)//green to blue maping vocal
-  volumeBar(other,color(19, 240, 63),color(19, 100, 240),300)//green to blue maping drum
+  if (song.currentTime() < 49 || song.currentTime() > 73) {
+    if (song.currentTime() > 0 && song.currentTime() < 16.5) {
+      volumeBar(bass, color(0), color(255), 0) //black to white maping bass
+      volumeBar(drum, color(0), color(255), 85) //black to white maping drum
+      volumeBar(vocal, color(0), color(255), 170) //black to white maping vocal
+      volumeBar(other, color(0), color(255), 250) //balck to white maping other
+
+
+    } else {
+      volumeBar(bass, color(240, 34, 19), color(240, 137, 19), 0) //red to orange maping bass
+      volumeBar(drum, color(19, 240, 63), color(19, 100, 240), 85) //green to blue maping drum
+      volumeBar(vocal, color(237, 26, 223), color(116, 25, 227), 170) //pink to purple maping vocal
+      volumeBar(other, color(0), color(255), 250) //balck to white maping other
+    }
+
+
+  }
+  else {
+    drawBullet(bullet1x, bullet1y); //first bullet 
+    bullet1x = bullet1x +5;
+    if (bullet1x > width) {
+      bullet1x = -100;
+
+    }
+
+    drawBullet(bullet2x, bullet2y); 
+    bullet2x = bullet2x +5;
+    if (bullet2x > width) {
+      bullet2x = -150;
+
+    }
+
+    drawBullet(bullet3x, bullet3y); 
+    bullet3x = bullet3x +5;
+    if (bullet3x > width) {
+      bullet3x = -200;
+
+    }
+
+
+    drawBullet(bullet4x, bullet4y); 
+    bullet4x = bullet4x +5;
+    if (bullet4x > width) {
+      bullet4x = -500;
+
+    }
+  }
+  
+
+  //if statment to pause music 
+  // if(song.currentTime() > 53&&song.currentTime() <70){
+
+
+  // }
+  // else {
+  // volumeBar(bass,color(240, 34, 19),color(240, 137, 19),0)//red to orange maping bass
+  // volumeBar(drum,color(19, 240, 63),color(19, 100, 240),85)//green to blue maping drum
+  // volumeBar(vocal,color(237, 26, 223),color(116, 25, 227),170)//pink to purple maping vocal
+  // volumeBar(other,color(0),color(255),250)//balck to white maping other
+
+  // }
 
 }
-function volumeBar(track, softColor, brightColor, xPosition){
 
-  let color1 = softColor;//color(237, 19, 19)red
-  let color2 = brightColor;//color(237, 113, 19)orange
+function drawBullet(x, y) {
+  push()
+  translate(x, y) // Position of top left corner
+  scale(0.5) // Scale of image
+  image(bullet, 0, 0)
+  pop()
+}
 
-  let volumeMapForColor = map(track,0,100,0,1);
+
+function volumeBar(track, softColor, brightColor, xPosition) {
+
+  let color1 = softColor; //color(237, 19, 19)red
+  let color2 = brightColor; //color(237, 113, 19)orange
+
+  let volumeMapForColor = map(track, 0, 100, 0, 1);
   let strokeColor = lerpColor(color1, color2, volumeMapForColor);
-  strokeWeight(3);
+  strokeWeight(5);
   stroke(strokeColor);
-  
-  let volumeMapForLines =map(track,0,100,30,90)
-  let lenghtOfLine = 30;
+
+  let volumeMapForLines = map(track, 0, 100, 30, 90)
+  let lenghtOfLine = 50;
   let lineStart = 100;
-  let lineEnd = lineStart+lenghtOfLine;
-   
-  
-for(let i =1; i <= volumeMapForLines; i++) {
-  let lineStep = i*10;
-  line(lineStart + xPosition, lineStep, lineEnd + xPosition, lineStep);
- 
-
-}
+  let lineEnd = lineStart + lenghtOfLine;
 
 
-
-  // var ballSize= 70;
-  // var bassMap= map(bass,0,100, 0+ballSize/2, ballSize); //map variable 
-  
-  
-
-  // fill(middleColor); 
-  // ellipse(width/2, 400, bassMap);
-  
-  // var ballSize= 40;
-  // var drumHeight= map(drum,0,100, 0+ballSize/2,height ); 
- 
-
-  // fill(250); 
-  // ellipse(width/2, drumHeight, ballSize);
+  for (let i = 1; i <= volumeMapForLines; i++) {
+    let lineStep = i * 10;
+    line(lineStart + xPosition, lineStep, lineEnd + xPosition, lineStep);
 
 
-
-  //  let bar_spacing = height / 10;
-  //  let bar_height = width / 12;
-  //  let bar_pos_x = width / 2;
+  }
 
  
-
-  //  // vocal bar is red
-  //  fill(200, 0, 0);
-  //  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-  //  fill(0);
-  //  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-  //  // drum bar is green
-  //  fill(0, 200, 0);
-  //  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-  //  fill(0);
-  //  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-  //  // bass bar is blue
-  //  fill(50, 50, 240);
-  //  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-  //  fill(0);
-  //  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-  //  // other bar is white
-  //  fill(200, 200, 200);
-  //  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-  //  fill(0);
-  //  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-  //  fill(255, 255, 0);
- 
-  //  // display "words"
-  //  textAlign(CENTER);
-  //  textSize(vocal);
-  //  text(words, width/2, height/3);
 }
